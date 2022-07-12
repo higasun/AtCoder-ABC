b742011eb38e662be5b5202111feea34279a63d0
Taichi Higasa <albatross@asagi.waseda.jp>
2022-07-12 16:13:09 +0900

    modified D.py

--- submissions/abc203/D.py
+++ submissions/abc203/D.py
@@ -6,7 +6,7 @@ for _ in range(n):
     a.append([0]+tmp+[0])
 a.append([0]*(n+2))
 
-#答えで二分探索
+#最適な中央値を二分探索
 l = 0
 r = 10**9+1
 while r - l > 1:
@@ -20,7 +20,7 @@ while r - l > 1:
             if a[i][j] >= m:
                 b[i][j] = 1
 
-    #二次元の累積和をとる
+    #二次元累積和
     #右方向
     for i in range(1,n+1):
         for j in range(1,n+1):
@@ -31,18 +31,19 @@ while r - l > 1:
         for i in range(1,n+1):
             b[i][j] += b[i-1][j]
     
-    check = 1
+    #check: 中央値がm未満となるようなカーネルが存在するか
+    check = False
     for i in range(1, n-k+2):
         for j in range(1, n-k+2):
             tmp = b[i+k-1][j+k-1] - b[i+k-1][j-1] - b[i-1][j+k-1] + b[i-1][j-1]
 
-            if tmp < k**2//2+1:
-                check = 0
+            if tmp < k**2//2+1: # <=> bのカーネル内の中央値が0 <=> aのカーネル内の中央値がm未満
+                check = True
 
-    if check == 1:
-        l = m
-    else:
+    if check: 
         r = m
+    else: 
+        l = m
 
 print(l)
 
